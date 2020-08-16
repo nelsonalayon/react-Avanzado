8db19ea1a5453f846fc594f0e22c06c420e3f80b
Nelson Alayón <nelson.alayon@gmail.com>
2020-08-16 12:59:58 -0500

    clase 14, se usa el useEffect para hacer un fetch de datos, pero dejo la funcion creada solamente porque no he podido subir la api a la base de datos

--- src/components/listOfCategories/index.js
+++ src/components/listOfCategories/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 import {Category} from '../category/'
 import { List, Item } from './styles'
 
-import { categories } from '../../../api/db.json'
+import { categories as mockCategories } from '../../../api/db.json'
+
+
 
 export const ListOfCategories = () => {
+
+    useEffect(function() {
+        fetch('')
+        .then(res=> res.json())
+        .then(response => {
+            setCategories(response)
+        })
+    }, [])
+
+    const [ categories, setCategories ] = useState(mockCategories)
     return <List>
         {
             categories.map(category => <Item key={category}>
