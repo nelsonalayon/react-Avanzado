1cd768eecb1bbb52cbd600fdd714f8a11db940ba
Nelson Alayón <nelson.alayon@gmail.com>
2020-08-15 20:46:10 -0500

    the heard icon was added from react-icons and it was create a list of photocards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {ListOfCategories} from './components/listOfCategories/'
 import { GlobalStyle } from './GlobalStyles'
-import {PhotoCard} from './components/photocard'
+import {ListOfPhotoCards} from './components/listOfPhotoCards'
 
 export const App = () => {
     return(
         <div>
             <GlobalStyle/>
             <ListOfCategories/>
-            <PhotoCard/>
+            <ListOfPhotoCards/>
         </div>
         
     )
--- src/components/photocard/index.js
+++ src/components/photocard/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { ImgWrapper, Img, Button } from './styles'
 
+import { MdFavoriteBorder } from 'react-icons/md'
+
 const DEFAULT_IMAGE = 'https://static.hsbnoticias.com/sites/default/files/styles/original/public/gallery/2018/04/gato-33.jpg'
 
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
@@ -13,7 +15,7 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
                 </ImgWrapper>
             </a>
             <Button>
-                {likes} likes!
+                 <MdFavoriteBorder size = '32px'/> {likes} likes!
             </Button>
         </article>
     )
