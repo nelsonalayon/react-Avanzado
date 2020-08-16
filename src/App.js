492dff6e0bd01998f57702deef6de3e69443f3db
Nelson Alayón <nelson.alayon@gmail.com>
2020-08-16 11:57:16 -0500

    clase 13, animaciones y como hacer un componente aparte para reutilizar la animacion cuando se requiera

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {ListOfCategories} from './components/listOfCategories/'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './components/syles/GlobalStyles'
 import {ListOfPhotoCards} from './components/listOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => {
     return(
         <div>
+            <Logo/> 
             <GlobalStyle/>
             <ListOfCategories/>
             <ListOfPhotoCards/>
