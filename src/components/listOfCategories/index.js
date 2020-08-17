30a21899020c4dc8f26b7d04003c7008e35b3e0a
Nelson Alayón <nelson.alayon@gmail.com>
2020-08-16 19:44:00 -0500

    se creo una barra superior que queda fix cuando el scroll se mueve a determinada posición con el hook useEffect()

--- src/components/listOfCategories/index.js
+++ src/components/listOfCategories/index.js
@@ -6,11 +6,12 @@ import { List, Item } from './styles'
 
 import { categories as mockCategories } from '../../../api/db.json'
 
+import { FadeIn } from '../syles/animation.js'
+
 
 
 export const ListOfCategories = () => {
-
-    useEffect(function() {
+     useEffect(function() {
         fetch('')
         .then(res=> res.json())
         .then(response => {
@@ -18,12 +19,37 @@ export const ListOfCategories = () => {
         })
     }, [])
 
-    const [ categories, setCategories ] = useState(mockCategories)
-    return <List>
+    useEffect(function() {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
+            setShowFixed(newShowFixed)
+        }
+        document.addEventListener('scroll', onScroll)
+
+        return () => document.removeEventListener('scroll', onScroll)
+    }, [showFixed])
+
+    const renderList = (fixed) => {
+      return  <List className = {fixed ? 'fixed' : ''}>
         {
             categories.map(category => <Item key={category}>
                 <Category {...category}/>
             </Item>)
         }
     </List>
+    }
+
+    const [showFixed, setShowFixed] = useState(false)
+
+    const [ categories, setCategories ] = useState(mockCategories)
+
+    
+    return (
+        <>
+        {renderList()}
+        {showFixed && renderList(true)}
+        </>
+        )
+
 }
